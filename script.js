fc54dce0d748184e2696a3e229ca2fdee38c791f
vaishnavi <148844241+vaish4596@users.noreply.github.com>
2026-02-23 12:30:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,51 @@
+const msg = new SpeechSynthesisUtterance();
+let voices = [];
+
+const voicesDropdown = document.querySelector('[name="voice"]');
+const options = document.querySelectorAll('[type="range"], [name="text"]');
+const speakButton = document.querySelector("#speak");
+const stopButton = document.querySelector("#stop");
+
+msg.text = document.querySelector('[name="text"]').value;
+
+// Populate Voices
+function populateVoices() {
+  voices = speechSynthesis.getVoices();
+
+  voicesDropdown.innerHTML = voices
+    .filter(voice => voice.lang.includes("en"))
+    .map(voice => 
+      `<option value="${voice.name}">
+        ${voice.name} (${voice.lang})
+      </option>`
+    )
+    .join('');
+}
+
+// Set Selected Voice
+function setVoice() {
+  msg.voice = voices.find(voice => voice.name === this.value);
+  toggle();
+}
+
+// Start / Stop Speaking
+function toggle(startOver = true) {
+  speechSynthesis.cancel();
+  if (startOver) {
+    msg.text = document.querySelector('[name="text"]').value;
+    speechSynthesis.speak(msg);
+  }
+}
+
+// Update Rate / Pitch / Text
+function setOption() {
+  msg[this.name] = this.value;
+  toggle();
+}
+
+// Event Listeners
+speechSynthesis.addEventListener('voiceschanged', populateVoices);
+voicesDropdown.addEventListener('change', setVoice);
+options.forEach(option => option.addEventListener('change', setOption));
+speakButton.addEventListener('click', toggle);
+stopButton.addEventListener('click', () => toggle(false));
